b182cdd40ea70f5dea6fd68dd7473727d622c92c
Senin Maksim <wecpo@yandex.ru>
2024-07-31 21:41:53 +0300

    added / total price

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function init() {
-  createChoiceProductForm();
-}
-
 function createChoiceProductForm() {
   let cart = [];
 
   const body = document.querySelector("body");
 
   // Создание кнопки добавления продукта в корзину
-  const addToCartButton = document.createElement("button");
-  addToCartButton.style.marginLeft = "10px";
-  addToCartButton.textContent = "Добавить в корзину";
-  addToCartButton.addEventListener("click", addToCart);
+  const addProductToCartButton = document.createElement("button");
+  addProductToCartButton.style.marginLeft = "10px";
+  addProductToCartButton.textContent = "Добавить в корзину";
+  addProductToCartButton.addEventListener("click", addProductToCart);
 
   // Создание формы выбора продукта
   const formChoiceProduct = document.createElement("form");
@@ -19,7 +15,6 @@ function createChoiceProductForm() {
 
   // Создание label для формы выбора продукта
   const labelForFormChoiceProduct = document.createElement("label");
-  labelForFormChoiceProduct.setAttribute("for", "formChoiceProduct");
   labelForFormChoiceProduct.textContent = "Выберите продукт ";
 
   // Создание выпадающего списка для формы выбора продукта
@@ -28,31 +23,34 @@ function createChoiceProductForm() {
 
   // Создание списка продуктов
   const listOfProducts = document.createElement("ol");
-  listOfProducts.textContent = "Ваша корзина: ";
+  listOfProducts.textContent = `Ваша корзина пуста`;
+
+  const totalCartPrice = document.createElement("div");
+  totalCartPrice.textContent = "";
 
   // Создание продуктов в выпадающем списке
   const option1 = document.createElement("option");
-  option1.value = "Гречка - 100р";
+  option1.value = "Гречка - 100";
   option1.textContent = "Гречка - 100р";
 
   const option2 = document.createElement("option");
-  option2.value = "Яблоки - 110р";
+  option2.value = "Яблоки - 110";
   option2.textContent = "Яблоки - 110р";
 
   const option3 = document.createElement("option");
-  option3.value = "Сливочное масло - 200р";
+  option3.value = "Сливочное масло - 200";
   option3.textContent = "Сливочное масло - 200р";
 
   const option4 = document.createElement("option");
-  option4.value = `Квас - 130р`;
+  option4.value = `Квас - 130`;
   option4.textContent = "Квас - 130р";
 
   const option5 = document.createElement("option");
-  option5.value = `Колбаса - 400р`;
+  option5.value = `Колбаса - 400`;
   option5.textContent = "Колбаса - 400р";
 
   const option6 = document.createElement("option");
-  option6.value = `Яйца - 140р`;
+  option6.value = `Яйца - 140`;
   option6.textContent = "Яйца - 140р";
 
   selectFormChoiceProduct.append(
@@ -68,35 +66,50 @@ function createChoiceProductForm() {
     formChoiceProduct,
     labelForFormChoiceProduct,
     selectFormChoiceProduct,
-    addToCartButton,
-    listOfProducts
+    addProductToCartButton,
+    listOfProducts,
+    totalCartPrice
   );
 
   function renderCart() {
+    // Функция обновления корзины
     const productsLi = document.querySelectorAll("li");
 
+    // Удаляем все элементы списка перед рендером
     for (let i = 0; i < productsLi.length; i++) {
       productsLi[i].remove();
     }
 
+    // Рендерим список продуктов
     for (let product of cart) {
-      const removeButton = document.createElement("button");
-      removeButton.style.marginLeft = "15px";
-      removeButton.textContent = "x";
-      removeButton.addEventListener("click", removeItemFromCart);
+      const removeProductButton = document.createElement("button");
+      removeProductButton.style.marginLeft = "15px";
+      removeProductButton.textContent = "x";
+      removeProductButton.addEventListener("click", removeProductFromCart);
 
       const li = document.createElement("li");
-      li.textContent = `${product.name} - ${product.price} ${product.count} шт.`;
+      li.textContent = `${product.name} - ${product.price}р ${product.count} шт.`;
       li.id = product.name;
-      li.append(removeButton);
+      li.append(removeProductButton);
 
       listOfProducts.append(li);
     }
+    cart.length > 0
+      ? (totalCartPrice.textContent = `Общая стоимость: ${cart.reduce(
+          (acc, item) => {
+            const itemPrice = +item.price * item.count;
+            console.log(item);
+            return (acc += itemPrice);
+          },
+          0
+        )}р`)
+      : (totalCartPrice.textContent = "");
   }
 
-  function addToCart(event) {
+  function addProductToCart(event) {
     event.preventDefault();
 
+    listOfProducts.textContent = `Ваша корзина:`;
     const productName = selectFormChoiceProduct.value.split("-")[0];
     const productPrice = selectFormChoiceProduct.value.split("-")[1];
 
@@ -119,12 +132,11 @@ function createChoiceProductForm() {
         price: productPrice,
         count: 1,
       });
-
       renderCart();
     }
   }
 
-  function removeItemFromCart(event) {
+  function removeProductFromCart(event) {
     const productName = event.target.parentElement.id;
 
     const product = cart.find((item) => item.name === productName);
@@ -136,6 +148,10 @@ function createChoiceProductForm() {
       });
 
       cart = newCart;
+
+      if (!cart.length) {
+        listOfProducts.textContent = "Ваша корзина пуста";
+      }
     } else {
       const indexOfProductInCart = cart.findIndex(
         (item) => item.name === productName
@@ -147,12 +163,10 @@ function createChoiceProductForm() {
       };
 
       cart[indexOfProductInCart] = updatedProduct;
-
-      renderCart();
     }
 
     renderCart();
   }
 }
 
-init();
+createChoiceProductForm();
