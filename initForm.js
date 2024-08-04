b83a93ab6fcd0b3fecc737cc2605ce900bc51f7d
Senin Maksim <wecpo@yandex.ru>
2024-08-04 17:58:01 +0300

    refactor: rewritten to prototypes, added remove product from cart function, creating form function was taken out to another module

---
+++ initForm.js
@@ -0,0 +1,93 @@
+import Order from "./script.js";
+
+const selectOptions = [
+  {
+    value: "Гречка - 100",
+    textContent: "Гречка - 100р",
+  },
+  {
+    value: "Яблоки - 110",
+    textContent: "Яблоки - 110р",
+  },
+  {
+    value: "Сливочное масло - 200",
+    textContent: "Сливочное масло - 200р",
+  },
+  {
+    value: "Квас - 130",
+    textContent: "Квас - 100р",
+  },
+  {
+    value: "Колбаса - 400",
+    textContent: "Колбаса - 400р",
+  },
+  {
+    value: "Яйца - 140",
+    textContent: "Яйца - 140р",
+  },
+];
+
+export default function initForm() {
+  const body = document.querySelector("body");
+
+  // Создание кнопки добавления продукта в корзину
+  const addProductToCartButton = document.createElement("button");
+  addProductToCartButton.style.marginLeft = "10px";
+  addProductToCartButton.textContent = "Добавить в корзину";
+  addProductToCartButton.addEventListener("click", (event) =>
+    Order.prototype.addProductToCart(event)
+  );
+  addProductToCartButton.addEventListener(
+    "unload",
+    () =>
+      addProductToCartButton.removeEventListener(
+        "click",
+        Order.prototype.addProductToCart
+      ),
+    {
+      once: true,
+    }
+  );
+
+  // Создание формы выбора продукта
+  const formChoiceProduct = document.createElement("form");
+  formChoiceProduct.name = "formChoiceProduct";
+
+  // Создание label для формы выбора продукта
+  const labelForFormChoiceProduct = document.createElement("label");
+  labelForFormChoiceProduct.textContent = "Выберите продукт";
+
+  // Создание выпадающего списка для формы выбора продукта
+  const selectFormChoiceProduct = document.createElement("select");
+  selectFormChoiceProduct.id = "selectFormChoiceProduct";
+  selectFormChoiceProduct.name = "product";
+
+  // Создание списка продуктов
+  const listOfProducts = document.createElement("ol");
+  listOfProducts.id = "listOfProducts";
+  listOfProducts.textContent = `Ваша корзина пуста`;
+
+  const totalCartPrice = document.createElement("div");
+  totalCartPrice.id = "totalCartPrice";
+  totalCartPrice.textContent = "";
+
+  selectFormChoiceProduct.append(...getSelectOptionsArray(selectOptions));
+
+  body.append(
+    formChoiceProduct,
+    labelForFormChoiceProduct,
+    selectFormChoiceProduct,
+    addProductToCartButton,
+    listOfProducts,
+    totalCartPrice
+  );
+}
+
+function getSelectOptionsArray(selectOptions) {
+  return selectOptions.map((item) => {
+    const option = document.createElement("option");
+    option.value = item.value;
+    option.textContent = item.textContent;
+    return option;
+  });
+}
--- script.js
+++ script.js
@@ -1,180 +1,127 @@
-const selectOptions = [
-  {
-    value: "Гречка - 100",
-    textContent: "Гречка - 100р",
-  },
-  {
-    value: "Яблоки - 110",
-    textContent: "Яблоки - 110р",
-  },
-  {
-    value: "Сливочное масло - 200",
-    textContent: "Сливочное масло - 200р",
-  },
-  {
-    value: "Квас - 130",
-    textContent: "Квас - 100р",
-  },
-  {
-    value: "Колбаса - 400",
-    textContent: "Колбаса - 400р",
-  },
-  {
-    value: "Яйца - 140",
-    textContent: "Яйца - 140р",
-  },
-];
+import initForm from "./initForm.js";
 
-function createChoiceProductForm() {
-  let cart = [];
+export default function Order() {
+  initForm();
+}
 
-  const body = document.querySelector("body");
+Order.prototype.cart = [];
 
-  // Создание кнопки добавления продукта в корзину
-  const addProductToCartButton = document.createElement("button");
-  addProductToCartButton.style.marginLeft = "10px";
-  addProductToCartButton.textContent = "Добавить в корзину";
-  addProductToCartButton.addEventListener("click", addProductToCart);
-  addProductToCartButton.addEventListener(
-    "unload",
-    () => addProductToCartButton.removeEventListener("click", addProductToCart),
-    {
-      once: true,
-    }
+Order.prototype.addProductToCart = function (event) {
+  // Функция добавления продукта в корзину
+  event.preventDefault();
+
+  const listOfProducts = document.querySelector("#listOfProducts");
+  listOfProducts.textContent = `Ваша корзина:`;
+
+  const selectFormChoiceProduct = document.querySelector(
+    "#selectFormChoiceProduct"
   );
 
-  // Создание формы выбора продукта
-  const formChoiceProduct = document.createElement("form");
-  formChoiceProduct.name = "formChoiceProduct";
+  const [productName, productPrice] =
+    selectFormChoiceProduct.value.split(" - ");
 
-  // Создание label для формы выбора продукта
-  const labelForFormChoiceProduct = document.createElement("label");
-  labelForFormChoiceProduct.textContent = "Выберите продукт ";
+  const productIndex = this.getProductIndex(productName);
 
-  // Создание выпадающего списка для формы выбора продукта
-  const selectFormChoiceProduct = document.createElement("select");
-  selectFormChoiceProduct.name = "product";
-
-  // Создание списка продуктов
-  const listOfProducts = document.createElement("ol");
-  listOfProducts.textContent = `Ваша корзина пуста`;
-
-  const totalCartPrice = document.createElement("div");
-  totalCartPrice.textContent = "";
-
-  function getSelectOptionsArray() {
-    return selectOptions.map((item) => {
-      const option = document.createElement("option");
-      option.value = item.value;
-      option.textContent = item.textContent;
-      return option;
+  if (productIndex >= 0) {
+    this.cart[productIndex].count++;
+  } else {
+    this.cart.push({
+      name: productName,
+      price: productPrice,
+      count: 1,
     });
   }
 
-  selectFormChoiceProduct.append(...getSelectOptionsArray());
+  this.renderCart();
+};
 
-  body.append(
-    formChoiceProduct,
-    labelForFormChoiceProduct,
-    selectFormChoiceProduct,
-    addProductToCartButton,
-    listOfProducts,
-    totalCartPrice
-  );
+Order.prototype.decrementProductInCart = function (event) {
+  const productName = event.target.parentElement.id;
 
-  function getTotalPrice() {
-    return cart.reduce((acc, item) => {
-      const itemPrice = +item.price * item.count;
+  const product = this.cart.find((product) => product.name === productName);
 
-      return (acc += itemPrice);
-    }, 0);
+  if (product.count === 1) {
+    this.removeProductFromCart(event);
+  } else {
+    const productIndex = this.getProductIndex(productName);
+
+    this.cart[productIndex].count--;
   }
 
-  function renderCart() {
-    // Функция обновления корзины
-    const productsLi = document.querySelectorAll("li");
+  this.renderCart();
+};
 
-    // Удаляем все элементы списка перед рендером
-    for (let i = 0; i < productsLi.length; i++) {
-      productsLi[i].firstElementChild.removeEventListener(
-        "click",
-        removeProductFromCart
-      );
+Order.prototype.removeProductFromCart = function (event) {
+  const productName = event.target.parentElement.id;
+  this.cart = this.cart.filter((product) => product.name !== productName);
 
-      productsLi[i].remove();
-    }
+  const listOfProducts = document.querySelector("#listOfProducts");
+  listOfProducts.textContent = "Ваша корзина пуста";
+  this.renderCart();
+};
 
-    // Рендерим список продуктов
-    for (let product of cart) {
-      const removeProductButton = document.createElement("button");
-      removeProductButton.style.marginLeft = "15px";
-      removeProductButton.textContent = "x";
+Order.prototype.renderCart = function () {
+  // Функция обновления корзины
+  const productsLi = document.querySelectorAll("li");
 
-      removeProductButton.addEventListener("click", removeProductFromCart);
+  // Удаляем все элементы списка перед рендером
+  for (let i = 0; i < productsLi.length; i++) {
+    productsLi[i].firstElementChild.removeEventListener(
+      "click",
+      this.decrementProductInCart
+    );
+    productsLi[i].firstElementChild.removeEventListener(
+      "click",
+      this.removeProductFromCart
+    );
 
-      const li = document.createElement("li");
-      li.textContent = `${product.name} - ${product.price}р ${product.count} шт.`;
-      li.id = product.name;
-      li.append(removeProductButton);
-
-      listOfProducts.append(li);
-    }
-
-    // Отображение общей стоимости
-    if (cart.length) {
-      totalCartPrice.textContent = `Общая стоимость: ${getTotalPrice()}р`;
-    } else {
-      totalCartPrice.textContent = "";
-    }
+    productsLi[i].remove();
   }
 
-  function getProductIndex(productName) {
-    return cart.findIndex((item) => item.name === productName);
+  // Рендерим список продуктов
+  for (let product of this.cart) {
+    const decrementProductButton = document.createElement("button");
+    decrementProductButton.style.marginLeft = "15px";
+    decrementProductButton.textContent = "-";
+    decrementProductButton.addEventListener("click", (event) =>
+      this.decrementProductInCart(event)
+    );
+
+    const removeProductButton = document.createElement("button");
+    removeProductButton.style.marginLeft = "15px";
+    removeProductButton.textContent = "x";
+    removeProductButton.addEventListener("click", (event) =>
+      this.removeProductFromCart(event)
+    );
+
+    const li = document.createElement("li");
+    li.textContent = `${product.name} - ${product.price}р ${product.count} шт.`;
+    li.id = product.name;
+    li.append(decrementProductButton, removeProductButton);
+
+    const listOfProducts = document.querySelector("#listOfProducts");
+    listOfProducts.append(li);
   }
 
-  function addProductToCart(event) {
-    // Функция добавления продукта в корзину
-    event.preventDefault();
-
-    listOfProducts.textContent = `Ваша корзина:`;
-
-    const [productName, productPrice] =
-      selectFormChoiceProduct.value.split("-");
-
-    const productIndex = getProductIndex(productName);
-
-    if (productIndex >= 0) {
-      cart[productIndex].count++;
-    } else {
-      cart.push({
-        name: productName,
-        price: productPrice,
-        count: 1,
-      });
-    }
-
-    renderCart();
+  // Отображение общей стоимости
+  const totalCartPrice = document.querySelector("#totalCartPrice");
+  if (this.cart.length) {
+    totalCartPrice.textContent = `Общая стоимость: ${this.getTotalPrice()}р`;
+  } else {
+    totalCartPrice.textContent = "";
   }
+};
 
-  function removeProductFromCart(event) {
-    const productName = event.target.parentElement.id;
+Order.prototype.getTotalPrice = function () {
+  return this.cart.reduce((acc, item) => {
+    const itemPrice = +item.price * item.count;
 
-    const product = cart.find((product) => product.name === productName);
+    return (acc += itemPrice);
+  }, 0);
+};
 
-    if (product.count === 1) {
-      cart = cart.filter((product) => product.name !== productName);
+Order.prototype.getProductIndex = function (productName) {
+  return this.cart.findIndex((item) => item.name === productName);
+};
 
-      if (!cart.length) {
-        listOfProducts.textContent = "Ваша корзина пуста";
-      }
-    } else {
-      const productIndex = getProductIndex(productName);
-
-      cart[productIndex].count--;
-    }
-
-    renderCart();
-  }
-}
-
-createChoiceProductForm();
+new Order();
